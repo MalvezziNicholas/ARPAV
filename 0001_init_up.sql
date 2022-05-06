0003e7d37fd10f62a9836a42d38fd698b1d12a5c
MalvezziNicholas <71836880+MalvezziNicholas@users.noreply.github.com>
2022-05-06 12:28:30 +0200

    Update 0001_init_up.sql

--- 0001_init_up.sql
+++ 0001_init_up.sql
@@ -1,20 +1,22 @@
 create DATABASE arpav;
 
+use arpav;
+
 CREATE table stazioni(
-    codseqst int(10) PRIMARY KEY AUTO_INCREMENT, 
+    id int PRIMARY KEY AUTO_INCREMENT, 
     nome varchar(32) not null,
     localita varchar(32) not null,
     comune varchar(32) not null,
     provincia varchar(32) not null,
-    lat float(3,2) not null,
-    lon float(3,2) not null
+    lat float(3, 2) not null,
+    lon float(3, 2) not null
 )
 
 CREATE table rilevazioni(
-    codseqst int(10) PRIMARY KEY AUTO_INCREMENT, 
+    id int PRIMARY KEY AUTO_INCREMENT, 
     data date not null,
     tipoInquinante varchar(32) not null,
     valore int not null,
-    FOREIGN KEY(codseqst) REFERENCES stazioni(codseqst)
+    FOREIGN KEY(id) REFERENCES stazioni(id)
 		ON UPDATE CASCADE ON DELETE CASCADE
 )
